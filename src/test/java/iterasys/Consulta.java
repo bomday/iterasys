a526bea1e2af067d379e11f2cc5b652a0f26633d
Day Lima <dayanecamilelima@gmail.com>
2024-04-25 13:07:01 -0300

    feat: new scenarios

--- src/test/java/iterasys/Consulta.java
+++ src/test/java/iterasys/Consulta.java
@@ -12,6 +12,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 // ctrl + shift + "o" para importar funcionalidades automaticamente
 
 public class Consulta {
@@ -20,7 +26,7 @@ public class Consulta {
 	String originalHandle;
 	WebDriver driver;
 	
-	@io.cucumber.java.Before
+	@Before
 	public void iniciar() {
 		// Estabelece url de pesquisa
 		url = "https://iterasys.com.br/pt/cursos-de-teste-e-qa/";
@@ -33,33 +39,12 @@ public class Consulta {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
-	@io.cucumber.java.After
+	@After
 	public void finalizar() {
 		driver.quit();
 	}
 	
-	@io.cucumber.java.en.Given("^cliente acessa site Iterasys$")
-	public void cliente_acessa_site_Iterasys() throws Throwable {
-		// Acessa URL
-	    driver.get(url);
-	    Thread.sleep(3000);
-	}
-	
-	@io.cucumber.java.en.When("^clicar no card do curso \"([^\"]*)\"$")
-	public void clicar_no_card_do_curso(String arg1) throws Throwable {
-		// Aceita cookies da página
-		driver.findElement(By.xpath("//span[contains(text(), 'ACEITAR')]")).click();
-		
-		// Captura o identificador da aba original
-	    originalHandle = driver.getWindowHandle();
-		
-		// Clica no card do curso
-	    driver.findElement(By.cssSelector("div.feature-nav-hover")).click();
-	    Thread.sleep(3000);
-	}
-	
-	@io.cucumber.java.en.Then("^apresenta pagina do curso na Udemy$")
-	public void apresenta_pagina_do_curso_na_Udemy() throws Throwable {
+	public void mudar_nova_aba() throws Throwable {
 		// Espera para que nova página seja aberta
 	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	    wait.until(ExpectedConditions.numberOfWindowsToBe(2));
@@ -74,6 +59,31 @@ public class Consulta {
                 break;
             }
         }
+	}
+	
+	@Given("^cliente acessa site Iterasys$")
+	public void cliente_acessa_site_Iterasys() throws Throwable {
+		// Acessa URL
+	    driver.get(url);
+	    Thread.sleep(3000);
+	}
+	
+	@When("^clicar no card do curso \"([^\"]*)\"$")
+	public void clicar_no_card_do_curso(String arg1) throws Throwable {
+		// Aceita cookies da página
+		driver.findElement(By.xpath("//span[contains(text(), 'ACEITAR')]")).click();
+		
+		// Captura o identificador da aba original
+	    originalHandle = driver.getWindowHandle();
+		
+		// Clica no card do curso
+	    driver.findElement(By.cssSelector("div.feature-nav-hover")).click();
+	    Thread.sleep(3000);
+	}
+	
+	@Then("^apresenta pagina do curso na Udemy$")
+	public void apresenta_pagina_do_curso_na_Udemy() throws Throwable {
+		mudar_nova_aba();
 
 	    // Recebe nova URL como valor
 	    String currentUrl = driver.getCurrentUrl();
@@ -84,5 +94,27 @@ public class Consulta {
 	    // Checa se título da página é "Início Rápido em Teste e QA"
 	    assertEquals("Início Rápido em Teste e QA", driver.findElement(By.cssSelector("h1.ud-heading-xl.clp-lead__title.clp-lead__title--small")).getText());
 	    Thread.sleep(3000);
+	}
+	
+	@When("clicar no item {string} no navbar")
+	public void clicar_no_item_no_navbar(String string) throws Throwable {
+		driver.findElement(By.xpath("//div[contains(text(), '" + string + "')]")).click();		
+		Thread.sleep(3000);
+	}
+
+	@Then("apresenta pagina {string}")
+	public void apresenta_pagina(String string) throws Throwable {
+		// Recebe nova URL como valor
+	    String currentUrl = driver.getCurrentUrl();
+	    
+	    // Checa se URL atual é a esperada
+	    if (string.equals("comunidade")) {
+	    	mudar_nova_aba();
+	    	assertTrue(currentUrl.contains("start-here"));
+	    } else {
+	    	assertTrue(currentUrl.contains(string));
+	    }
+	    
+		Thread.sleep(3000);
 	}
 }
--- target/test-classes/Consulta.feature
+++ target/test-classes/Consulta.feature
@@ -1,6 +1,34 @@
-Feature: Consulta de Curso (19/04/2024)
+Feature: Caminho Feliz (19/04/2024)
 
-  Scenario: Caminho feliz
+  Scenario: Acessar pagina curso "Inicio rapido em teste e QA"
     Given cliente acessa site Iterasys
     When clicar no card do curso "Inicio rapido em teste e QA"
     Then apresenta pagina do curso na Udemy
+    
+  Scenario: Abrir aba "Exames"
+    Given cliente acessa site Iterasys
+    When clicar no item "Exames" no navbar
+    Then apresenta pagina "exames"
+
+# Trabalhando em cenário    
+# Scenario: Abrir aba "Eventos"
+#   Given cliente acessa site Iterasys
+#   When clicar no item "Eventos" no navbar
+#   Then apresenta pagina "Eventos"
+
+# Trabalhando em cenário   
+#  Scenario: Abrir aba "Comunidade"
+#    Given cliente acessa site Iterasys
+#    When clicar no item "Comunidade" no navbar
+#    Then apresenta pagina "comunidade"
+    
+    
+  Scenario: Abrir aba "Sobre"
+    Given cliente acessa site Iterasys
+    When clicar no item "Sobre" no navbar
+    Then apresenta pagina "sobre"
+    
+  Scenario: Abrir aba "Contato"
+    Given cliente acessa site Iterasys
+    When clicar no item "Contato" no navbar
+    Then apresenta pagina "contato"
